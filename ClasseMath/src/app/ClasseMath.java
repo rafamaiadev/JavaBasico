8c7745a0c0500a55181dbfcde7be1cff997b1122
rafamaiadev <rafa.maia27@hotmail.com>
2023-03-12 11:10:08 -0300

    Número aleatório

---
+++ ClasseMath/src/app/ClasseMath.java
@@ -0,0 +1,10 @@
+package app;
+
+public class ClasseMath {
+    public static void main(String[] args) {
+        double ale = Math.random();
+        int num = (int) (ale*10);
+        System.out.println("O numero gerado é: " + num);
+
+    }
+}
