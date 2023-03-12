3a7c93ed15c7416c6f93c853072e720f8441461b
rafamaiadev <rafa.maia27@hotmail.com>
2023-03-12 20:16:12 -0300

    for earch

---
+++ Vetor02.java
@@ -0,0 +1,11 @@
+package app;
+
+public class Vetor02 {
+    public static void main(String[] args) {
+        double[] v = {2.5,3.5,4.5,5.5,6.5};
+        //Usando for each
+        for(double i: v) {
+            System.out.println(i);
+        }
+    }
+}
