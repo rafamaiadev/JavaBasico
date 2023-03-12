55ec448743e9b5bc4dbe1df63eb7adfd6b43a73a
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-03-11 22:00:30 -0300

    att

---
+++ TesteTipos.java
@@ -0,0 +1,14 @@
+package app;
+
+public class TesteTipos {
+    public static void main(String[] args) {
+        // conversão de string em inteiro.
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.println(valor);
+
+        String valor1 = "30";
+        int idade1 = Integer.parseInt(valor1);
+        System.out.println(idade);
+    }
+}
