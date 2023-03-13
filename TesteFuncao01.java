c2ba4fefe66e661c71d76ab99803a5806de5ea35
rafamaiadev <rafa.maia27@hotmail.com>
2023-03-12 21:03:02 -0300

    att

---
+++ TesteFuncao01.java
@@ -0,0 +1,12 @@
+package app;
+
+public class TesteFuncao01 {
+    static int soma(int a, int b) {
+        int s = a+b;
+        return s;
+    }
+    public static void main(String[] args) {
+        int sm = soma(20,30);
+        System.out.println(sm);
+    }
+}
